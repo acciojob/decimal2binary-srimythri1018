b2562c00a7d4ba66d55c0c02ea206bafe3c2710d
srimythri1018 <115878123+srimythri1018@users.noreply.github.com>
2023-05-08 23:03:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
-    function decToBinary(n)
-{
-	let binaryNum = new Array(32);
- 
-    // counter for binary array
-    let i = 0;
-    while (n > 0) {
- 
-        // storing remainder in binary array
-        binaryNum[i] = n % 2;
-        n = Math.floor(n / 2);
-        i++;
+function threeSum(S, target) {
+  // Sort the array in ascending order
+  S.sort(function(a, b) {
+    return a - b;
+  });
+  
+  // Initialize variables to store the closest sum and its difference from the target
+  var closestSum = Infinity;
+  var closestDiff = Infinity;
+  
+  // Loop through the array from the first element up to the third-to-last element
+  for (var i = 0; i < S.length - 2; i++) {
+    // Initialize pointers to the elements to the right of the current element
+    var j = i + 1;
+    var k = S.length - 1;
+    
+    // Loop through the remaining elements using the two pointers
+    while (j < k) {
+      // Calculate the sum of the three elements
+      var sum = S[i] + S[j] + S[k];
+      
+      // Calculate the difference between the sum and the target
+      var diff = Math.abs(sum - target);
+      
+      // Update the closest sum and difference if the current sum is closer to the target
+      if (diff < closestDiff) {
+        closestSum = sum;
+        closestDiff = diff;
+      }
+      
+      // Move the pointers towards each other based on the current sum's relation to the target
+      if (sum < target) {
+        j++;
+      } else if (sum > target) {
+        k--;
+      } else {
+        // If the sum is equal to the target, we have found the solution
+        return closestSum;
+      }
     }
- 
-    // printing binary array in reverse order
-    for (let j = i - 1; j >= 0; j--)
-        document.write(binaryNum[j]);
-}
- 
-// Driver program to test above function
-    let n = 17;
-    decToBinary(n);// write your code here
+  }
+  
+  // Return the closest sum found
+  return closestSum;
+
+	// write your code here
   
 }
 
-
+module.exports = threeSum;
